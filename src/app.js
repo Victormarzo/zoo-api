4d5692a3bde27c135ba5825261c90a668f03569a
victormarzo <victor.marzo.monteiro@gmail.com>
2025-11-22 15:20:57 -0300

    feat: Rota de criacao e obtencao de animais

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { testConnection } from './config/database.js';
+import animalRoutes from './routes/animal-routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -28,6 +29,8 @@ app.get('/db-test', async (req, res) => {
         });
     }
 });
+
+app.use('/api/animais', animalRoutes)
 
 app.listen(PORT, () => {
     console.log(`Express server running at http://localhost:${PORT}`);
